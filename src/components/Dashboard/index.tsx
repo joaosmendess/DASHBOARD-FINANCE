2024cfa4642cbe6ea57ba713a2811481b0b39258
Joao Mendes <joaosilva0721@gmail.com>
2023-04-11 21:48:02 -0300

    criando o content header do dashboard

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,15 +1,83 @@
 import React from "react";
 import { Container } from "./style";
 import ContentHeader from "../../pages/ContentHeader";
+import { useState } from "react";
+import { useMemo } from "react";
+import expenses from "../../repositories/expenses";
+import gains from "../../repositories/gains";
+import listOfMonths from '../../utils/months'
+
 
 import SelectInput from '../../pages/SelectInput'
 const Dashboard: React.FC = () => {
+
+  const [monthSelected, setMonthSelected] = useState<number>
+  (new Date().getMonth() + 1)
+;
+const [yearSelected, setYearSelected] = useState<number>
+  (new Date().getFullYear())
+;
   
-  const months = [
-    { value: 7, label: "julho" },
-    { value: 8, label: "Agosto" },
-    { value: 9, label: "Setembro" },
-  ];
+
+const months = useMemo(() => {
+  return listOfMonths.map((month, index) => {
+    return {
+      value: index + 1,
+      label: month,
+    };
+  });
+}, []);
+
+const years = useMemo(() => {
+  let uniqueYears: number[] = [];
+
+  
+
+  [...expenses, ...gains].forEach((item) => {
+    const date = new Date(item.date);
+    const year = date.getFullYear();
+
+    if (!uniqueYears.includes(year)) {
+      uniqueYears.push(year);
+    }
+  });
+  return uniqueYears.map((year) => {
+    return {
+      value: year,
+      label: year,
+    };
+  });
+}, []);
+
+
+
+
+  const handleMonthSelected = (month: string) => {
+
+
+  
+    try {
+          const parseMonth = Number(month);
+          setMonthSelected(parseMonth);
+    }catch(error){
+      throw new Error ('invalid month value. Is accept 0 - 24.')
+         
+    }
+  };
+
+
+  const handleYearSelected = (year: string) => {
+    try {
+          const parseYear = Number(year);
+          setYearSelected(parseYear);
+    }catch(error){
+      throw new Error ('invalid year value. Is accept integer numbers.')
+         
+    }
+  };
+
+
+  
 
   const options = [
     { value: 'maria', label: 'maria' },
@@ -19,7 +87,16 @@ const Dashboard: React.FC = () => {
   return (
     <Container>
     <ContentHeader title="Dashboard" lineColor="#F7931B">
-      <SelectInput options={options} onChange={() => {}}/>
+    <SelectInput
+          options={months}
+          onChange={(e) => handleMonthSelected(e.target.value)}
+          defaultValue={monthSelected}
+        />
+        <SelectInput
+          options={years}
+          onChange={(e) => handleYearSelected(e.target.value)}
+          defaultValue={yearSelected}
+        />
       
 
     </ContentHeader>
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -31,12 +31,12 @@ interface IData {
 const List: React.FC = () => {
   const [data, setData] = useState<IData[]>([]);
 
-  const [monthSelected, setMonthSelected] = useState<string>(
-    String(new Date().getMonth() + 1)
-  );
-  const [yearSelected, setYearSelected] = useState<string>(
-    String(new Date().getFullYear())
-  );
+  const [monthSelected, setMonthSelected] = useState<number>
+    (new Date().getMonth() + 1)
+  ;
+  const [yearSelected, setYearSelected] = useState<number>
+    (new Date().getFullYear())
+;
 
   const [frequencyFilterSelected, setfrequencyFilterSelected] = useState([
     "recorrente",
@@ -52,7 +52,7 @@ const List: React.FC = () => {
     return type === "entry-balance"
       ? {
           title: "Entradas",
-          lineColor: " #F7931B",
+          lineColor: " #4E41F0",
           data: gains,
         }
       : {
@@ -107,14 +107,35 @@ const List: React.FC = () => {
     }
   };
 
+  const handleMonthSelected = (month: string) => {
+    try {
+          const parseMonth = Number(month);
+          setMonthSelected(parseMonth);
+    }catch(error){
+      throw new Error ('invalid month value. Is accept 0 - 24.')
+         
+    }
+  };
+
+
+  const handleYearSelected = (year: string) => {
+    try {
+          const parseYear = Number(year);
+          setYearSelected(parseYear);
+    }catch(error){
+      throw new Error ('invalid year value. Is accept integer numbers.')
+         
+    }
+  };
+
   useEffect(() => {
 
 const {data} = pageData
 
     const filteredData = data.filter((item) => {
       const date = new Date(item.date);
-      const month = String(date.getMonth() + 1);
-      const year = String(date.getFullYear());
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
 
       return (
         month === monthSelected &&
@@ -142,12 +163,12 @@ const {data} = pageData
       <ContentHeader title={pageData.title} lineColor={pageData.lineColor}>
         <SelectInput
           options={months}
-          onChange={(e) => setMonthSelected(e.target.value)}
+          onChange={(e) => handleMonthSelected(e.target.value)}
           defaultValue={monthSelected}
         />
         <SelectInput
           options={years}
-          onChange={(e) => setYearSelected(e.target.value)}
+          onChange={(e) => handleYearSelected(e.target.value)}
           defaultValue={yearSelected}
         />
       </ContentHeader>
